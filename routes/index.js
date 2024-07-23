38fff3b82229bfee44472c945a5d7d801c5760c8
roshith <roshithprakash7@gmail.com>
2024-07-23 15:12:44 +0530

    Express Project Template Files.

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+import { Router } from "express"
+import { getName } from "../controllers/index.js"
+
+// Create a router.
+const router = Router()
+
+// Default route to check if auth routes are accessible.
+router.get("/", (req, res) => {
+    res.status(200).send({ data: "Default Route" })
+})
+
+// Example Route
+router.get("/getName", getName)
+
+export default router
